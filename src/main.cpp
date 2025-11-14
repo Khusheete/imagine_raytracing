95372ca0c1c3e28b4a973aeb0be9f08b27aeb0d2
Souchet Ferdinand <Siandfrance@gmail.com>
2025-11-14 23:15:31 +0100

    Updated license text

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,40 @@
+/* ------------------------------------------------------------------------------------------------------------------ *
+*                                                                                                                     *
+*                                                                                                                     *
+*                                                /\                    ^__                                            *
+*                                               /#*\  /\              /##@>                                           *
+*                                              <#* *> \/         _^_  \\    _^_                                       *
+*                                               \##/            /###\ \è\  /###\                                      *
+*                                                \/ /\         /#####n/xx\n#####\                                     *
+*                   Ferdinand                       \/         \###^##xXXx##^###/                                     *
+*                        Souchet                                \#/ V¨\xx/¨V \#/                                      *
+*                     (aka. @Khusheete)                          V     \c\    V                                       *
+*                                                                       //                                            *
+*                                                                     \o/                                             *
+*             ferdinand.souchet@etu.umontpellier.fr                    v                                              *
+*                                                                                                                     *
+*                                                                                                                     *
+*                                                                                                                     *
+*                                                                                                                     *
+* Copyright 2025 Ferdinand Souchet (aka. @Khusheete)                                                                  *
+*                                                                                                                     *
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated        *
+* documentation files (the “Software”), to deal in the Software without restriction, including without limitation the *
+* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to     *
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:                     *
+*                                                                                                                     *
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of    *
+* the Software.                                                                                                       *
+*                                                                                                                     *
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO    *
+* THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, *
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE      *
+* SOFTWARE.                                                                                                           *
+*                                                                                                                     *
+* ------------------------------------------------------------------------------------------------------------------ */
+
+
 #include <GL/gl.h>
 #include <algorithm>
 #include <cmath>
@@ -331,7 +368,7 @@ void ray_trace_from_camera() {
           const Ray ray = Ray(camera_position, ray_direction);
 
           // const Vec3 color = scenes[selected_scene].ray_trace(ray);
-          const Vec3 color = scenes[selected_scene].ray_trace_recursive(ray, 4);
+          const Vec3 color = scenes[selected_scene].ray_trace_recursive(rng, ray, 0);
 
           image(p_exec_index) += color;
         }
